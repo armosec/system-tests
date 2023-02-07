4eaba6fe022a641d9f33a6f2f52389619182bf36
Amir Malka <amirm@armosec.io>
2023-02-07 15:57:21 +0200

    temporary - point at fork for test

--- tests_scripts/kubescape/base_kubescape.py
+++ tests_scripts/kubescape/base_kubescape.py
@@ -277,7 +277,13 @@ class BaseKubescape(BaseK8S):
         else:
             os_name = "ubuntu"
 
-        download_url = f"https://github.com/{org}/kubescape/releases/{release}/download/kubescape-{os_name}-latest"
+        # download_url = f"https://github.com/{org}/kubescape/releases/{release}/download/kubescape-{os_name}-latest"
+
+        # TODO: REVERT BEFORE MERGING!!!!
+        download_url = 'https://api.github.com/repos/dwertent/kubescape/releases/latest'
+        res = requests.get(download_url).json()
+        version = res['tag_name']
+        download_url = f"https://github.com/dwertent/kubescape/releases/{release}/download/kubescape-{os_name}-latest-{version}"
 
         kubescape_exec = os.path.join(self.test_driver.temp_dir, "kubescape")
         res = requests.get(download_url)
