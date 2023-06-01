d396776152f92a5eaa1d2c1e0b18350287dbb226
kooomix <eranm@armosec.io>
2023-06-01 14:28:51 +0300

    proxy block traffic from kubescape namespace pods

--- configurations/helm-proxy/create_configmaps.sh
+++ configurations/helm-proxy/create_configmaps.sh
@@ -7,5 +7,7 @@ kubectl create configmap apache-config --from-file=$my_dir/httpd.conf
 kubectl apply -f $my_dir/httpd.deploy.yaml
 kubectl apply -f $my_dir/httpd.svc.yaml
 
+kubectl apply -f $my_dir/network_policy_block_all.yaml
+
 # kubectl apply -f $my_dir/nginx.svc.yaml
 kubectl rollout restart deployment httpd-proxy
