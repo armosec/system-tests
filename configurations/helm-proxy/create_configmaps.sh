6d6475b15c052c5d6fc8d3101be5d49164e93dae
kooomix <eranm@armosec.io>
2023-06-11 07:16:04 +0300

    handle errors

--- configurations/helm-proxy/create_configmaps.sh
+++ configurations/helm-proxy/create_configmaps.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 my_dir=`dirname $0`
 
-# create a configmap from a file
+# delete apache-config configmap if exists
 kubectl delete configmap apache-config
-kubectl create configmap apache-config --from-file=$my_dir/httpd.conf
-kubectl apply -f $my_dir/httpd.deploy.yaml
-kubectl apply -f $my_dir/httpd.svc.yaml
 
-kubectl apply -f $my_dir/network_policy_block_all.yaml
+# create apache-config configmap
+output=$(kubectl create configmap apache-config --from-file=$my_dir/httpd.conf 2>&1)
 
-# kubectl apply -f $my_dir/nginx.svc.yaml
-kubectl rollout restart deployment httpd-proxy
+if [ $? -ne 0 ]; then
+    echo "failed to create apache-config configmap: $?"
+    exit $?
+fi
+echo $output
+
+output=$(kubectl apply -f $my_dir/httpd.deploy.yaml 2>&1)
+
+if [ $? -ne 0 ]; then
+    echo "failed to deploy httpd-proxy: $?"
+    exit $?
+fi
+echo $output
+
+output=$(kubectl apply -f $my_dir/httpd.svc.yaml 2>&1)
+
+if [ $? -ne 0 ]; then
+    echo "failed to deploy httpd-proxy service: $?"
+    exit $?
+fi
+echo $output
+
+
+output=$(kubectl rollout restart deployment httpd-proxy 2>&1)
+
+if [ $? -ne 0 ]; then
+    echo "failed to restart restart deployment httpd-proxy: $?"
+    exit $?
+fi
+echo $output
