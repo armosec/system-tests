d85c71340ca9f3686662f61d39cfb4125b540bac
kooomix <eranm@armosec.io>
2023-06-13 10:55:01 +0300

    update python version

--- create_env.sh
+++ create_env.sh
@@ -2,8 +2,8 @@
 
 echo "user: $USER"
 echo "path: $PATH"
-python3 --version
-pip3 --version
+python3.8 --version
+pip3.8 --version
 docker --version
 
 #kubectl version --short
@@ -23,7 +23,7 @@ SCRIPTPATH="$(
 
 echo "Path: ${SCRIPTPATH}"
 
-if ! python3 -m venv systests_python_env; then
+if ! python3.8 -m venv systests_python_env; then
   echo "Failed to create python environment"
   exit 1
 fi
@@ -39,12 +39,12 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install openssl
   brew link openssl --force
   source systests_python_env/bin/activate
-  LDFLAGS="-L$(brew --prefix openssl@1.1)/lib" CFLAGS="-I$(brew --prefix openssl@1.1)/include" pip3 install -r requirements.txt
+  LDFLAGS="-L$(brew --prefix openssl@1.1)/lib" CFLAGS="-I$(brew --prefix openssl@1.1)/include" pip3.8 install -r requirements.txt
   brew unlink openssl
 else
   echo "OS: Linux"
   . systests_python_env/bin/activate
-  pip3 install -r requirements.txt
+  pip3.8 install -r requirements.txt
   wget https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-Linux-x86_64.sh -O cmake.sh
   sudo sh cmake.sh --prefix=/usr/local/ --exclude-subdir
 fi
