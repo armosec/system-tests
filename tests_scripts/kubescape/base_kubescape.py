f65982e2f50d5eb0762c69d2d490f3f988458571
kooomix <eranm@armosec.io>
2023-07-27 09:14:42 +0300

    check security fw

--- tests_scripts/kubescape/base_kubescape.py
+++ tests_scripts/kubescape/base_kubescape.py
@@ -1002,6 +1002,12 @@ class BaseKubescape(BaseK8S):
     def test_backend_vs_kubescape_result(self, report_guid, kubescape_result):
         be_frameworks = self.get_posture_frameworks(report_guid=report_guid)
 
+        # check if there are also security fw scanned for report_guid
+        for sf in statics.SECURITY_FRAMEWORKS:
+            be__current_security_framework = self.get_posture_frameworks(report_guid=report_guid, framework_name=sf)
+            if be__current_security_framework:
+                be_frameworks.append(be__current_security_framework)
+
         assert _CLI_SUMMARY_DETAILS_FIELD in kubescape_result, "expected key {} is not in kubescape result,kubescape_result: {}".format(
             _CLI_SUMMARY_DETAILS_FIELD, kubescape_result)
         kbs_r = kubescape_result[_CLI_SUMMARY_DETAILS_FIELD]
