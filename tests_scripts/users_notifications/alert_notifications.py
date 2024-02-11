30d025c4f934ac7f65b073c9d9b7b35c60797a22
refaelm <refaelm@armosec.io>
2024-02-11 17:25:47 +0200

    change env var name
    Signed-off-by: refaelm <refaelm@armosec.io>

--- tests_scripts/users_notifications/alert_notifications.py
+++ tests_scripts/users_notifications/alert_notifications.py
@@ -55,7 +55,7 @@ def get_messages_from_teams_channel(before_test):
 
 def get_messages_from_slack_channel(before_test):
     Logger.logger.info('Attempting to read messages from slack before timestamp ' + str(before_test))
-    client = WebClient(token=os.getenv("SLACK_TOKEN"))
+    client = WebClient(token=os.getenv("SLACK_SYSTEM_TEST_TOKEN"))
     result = client.conversations_history(channel=f'{os.getenv("SLACK_CHANNEL_ID")}', oldest=before_test)
     return result['messages']
 
