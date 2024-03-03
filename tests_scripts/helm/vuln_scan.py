1210da338229e017fce9506fc9858aa59b890183
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2024-03-03 19:53:14 +0200

    log
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- tests_scripts/helm/vuln_scan.py
+++ tests_scripts/helm/vuln_scan.py
@@ -889,7 +889,7 @@ class VulnerabilityV2Views(BaseVulnerabilityScanning):
             }]}
         wl_list, _ = self.wait_for_report(timeout=600, report_type=self.backend.get_vuln_v2_workloads,
                                               body=body,expected_results=1)  
-        print(wl_list)
+        Logger.logger.info("received httpd-proxy scan {}".format(wl_list))
 
         Logger.logger.info('2.1 get httpd-proxy workload with filteres and compare with expected')
         body =  {"innerFilters": [{   
