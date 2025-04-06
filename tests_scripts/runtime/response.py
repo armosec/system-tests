aec4f53fb7c23a39044315c5aa05da2c227c256a
kooomix <eranm@armosec.io>
2025-04-06 16:26:32 +0300

    feat: enhance incident response handling with applied status tracking

--- tests_scripts/runtime/response.py
+++ tests_scripts/runtime/response.py
@@ -20,11 +20,11 @@ class IncidentResponse(Incidents):
         cluster, namespace = self.setup(apply_services=False)
 
 
-        Logger.logger.info(". Install armo helm-chart before application so we will have final AP")
-        self.add_and_upgrade_armo_to_repo()
-        self.install_armo_helm_chart(helm_kwargs=self.helm_kwargs)
-        self.wait_for_report(self.verify_running_pods, sleep_interval=5, timeout=360,
-                             namespace=statics.CA_NAMESPACE_FROM_HELM_NAME)
+        # Logger.logger.info(". Install armo helm-chart before application so we will have final AP")
+        # self.add_and_upgrade_armo_to_repo()
+        # self.install_armo_helm_chart(helm_kwargs=self.helm_kwargs)
+        # self.wait_for_report(self.verify_running_pods, sleep_interval=5, timeout=360,
+        #                      namespace=statics.CA_NAMESPACE_FROM_HELM_NAME)
 
 
         # namespace = self.create_namespace()
@@ -38,35 +38,62 @@ class IncidentResponse(Incidents):
         # testing happy flow - order is important because, for example, if we apply seccomp profile and then kill process, the kill process will fail
         tests_to_body = {
             "ApplyNetworkPolicy": {
+                "body": {
                 "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_APPLY_NETWORK_POLICY,
                 "networkPolicyKind": "kubernetes"
+                },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_SUCCESS
             },
             "KillProcess": {
+                "body": {
                 "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_KILL            
                 },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_SUCCESS
+            },
             "PauseContainer": {
+                "body": {
                 "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_PAUSE            
                 },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_SUCCESS
+            },
             "StopContainer": {
+                "body": {
                 "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_STOP
                 },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_SUCCESS
+            },
             "ApplySeccompProfile": {
+                "body": {
                 "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_APPLY_SECCOMP_PROFILE            
+                },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_SUCCESS
+            },
+             "KillProcess": {
+                "body": {
+                "responseType": statics.RUNTIME_INCIDENT_RESPONSE_TYPE_KILL            
+                },
+                "expected_applied_status": statics.RUNTIME_INCIDENT_APPLIED_STATUS_FAILED
             },
         }
 
         tests_to_errors = {}
 
-        for test_name, body in tests_to_body.items():
+        for test_name, test_body in tests_to_body.items():
+            body = test_body["body"]
+            expected_applied_status = test_body["expected_applied_status"]
+
+            Logger.logger.info(f"Testing {test_name} with body {body}")
             try:
-                Logger.logger.info(f"Testing {test_name} with body {body}")
-                self.response_and_assert(incident_guid=inc["guid"], body=body)
+                self.response_and_assert(incident_guid=inc["guid"], body=body, expected_applied_status=expected_applied_status)
+                Logger.logger.info(f"Test {test_name} with body {body} was successful")
             except Exception as e:
                 Logger.logger.error(f"Failed to test {test_name} with body {body}, got exception {e}")
                 tests_to_errors[test_name] = str(e)
 
+
         if len(tests_to_errors) > 0:
-            Logger.logger.error(f"Failed to test the following tests: {tests_to_errors}")
+            for test_name, error in tests_to_errors.items():
+                Logger.logger.error(f"Test {test_name} failed with error: {error}")
             raise Exception(f"Failed to test the following tests: {tests_to_errors}")
 
         
@@ -75,7 +102,7 @@ class IncidentResponse(Incidents):
     def cleanup(self, **kwargs):
         return statics.SUCCESS, "Cleanup done"
 
-    def response_and_assert(self, incident_guid, body):
+    def response_and_assert(self, incident_guid, body, expected_applied_status):
         """
         This function is used to send a response to an incident and assert the response.
         :param incident_guid: The GUID of the incident to respond to.
@@ -83,12 +110,12 @@ class IncidentResponse(Incidents):
         :return: The response from the backend.
         """
         Logger.logger.info(f"Response to incident {incident_guid} with body {body}")
-        _ = self.backend.response_incident(incident_id=incident_guid, body=body)
+        _ = self.backend.response_incident(incident_id=incident_guid, body=body, expected_applied_status=expected_applied_status)
         
         auditlog, _ = self.wait_for_report(self.verify_audit_log, timeout=120, sleep_interval=10,
                                             incident_guid=incident_guid, action=body["responseType"])
     
-    def verify_audit_log(self, incident_guid, action):
+    def verify_audit_log(self, incident_guid, action, expected_applied_status):
         Logger.logger.info("Get incidents list")
 
         if action == statics.RUNTIME_INCIDENT_RESPONSE_TYPE_KILL:
@@ -117,7 +144,7 @@ class IncidentResponse(Incidents):
 
         response = self.backend.audit_log_incident(incident_id=incident_guid, body=body)
         incs = response['response']
-        assert len(incs) == 2, f"Failed to get incidents list {json.dumps(incs)}"
+        assert len(incs) == 2, f"Failed to get incidents list, expected 2 results but got {len(incs)}: {json.dumps(incs)}"
 
         init_log = incs[0]
         Logger.logger.info(f"verify Initiated audit log of action {action} and incident guid {incident_guid}:")
@@ -132,6 +159,6 @@ class IncidentResponse(Incidents):
         assert applied_log["details"]["action"] == action, f"Failed to get incidents list {json.dumps(incs)}"
         assert applied_log["eventOwner"]["incidentGUID"] == incident_guid, f"Failed to get incidents list {json.dumps(incs)}"
         assert applied_log["details"]["event"] == "Applied", f"Expected event Applied but got {applied_log['details']['event']}"
-        assert applied_log["details"]["status"] == "Success", f"Expected status Success but got {applied_log['details']['status']}"
+        assert applied_log["details"]["status"] == expected_applied_status, f"Expected status {expected_applied_status} but got {applied_log['details']['status']}"
         return incs
         
