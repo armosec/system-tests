019ad2b0e87e3b1337fd5c101a7408ccfc7607b9
Afek Berger <afekb@armosec.io>
2025-04-17 16:59:58 +0300

    added cadr test
    Signed-off-by: Afek Berger <afekb@armosec.io>

---
+++ tests_scripts/runtime/cadr.py
@@ -0,0 +1,78 @@
+
+from systest_utils import Logger, statics
+from tests_scripts.runtime.incidents import Incidents
+from os.path import join
+import json
+
+
+class CADRIncidents(Incidents):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        """
+        agenda:
+
+        1. Install armo helm-chart before application so we will have final AP
+        2. Apply deployments to namespaces
+        3. Verify running pods in namespaces
+        4. Simulate unexpected process in namespaces
+        5. Send mock data of cdr alerts combining the incident response with the unexpected process
+        6. Verify the incident data in the backend
+        7. Open CDR incident and verify the alerts
+        """
+
+    def start(self):
+        assert self.backend is not None, f"the test {self.test_driver.test_name} must run with backend"
+
+        cluster, namespace = self.setup()
+        Logger.logger.info("1. Install armo helm-chart before application so we will have final AP")
+        self.add_and_upgrade_armo_to_repo()
+        self.install_armo_helm_chart(helm_kwargs=self.helm_kwargs)
+        self.wait_for_report(self.verify_running_pods, sleep_interval=5, timeout=360,
+                             namespace=statics.CA_NAMESPACE_FROM_HELM_NAME)
+        wlids = self.deploy_and_wait(deployments_path=self.test_obj["deployments"], cluster=cluster, namespace=namespace)
+        self.create_application_profile(wlids=wlids, namespace=namespace, commands=[""])
+        self._test_unexpected_process(wlids=wlids, command="cat /etc/hosts", cluster=cluster, namespace=namespace)
+        return self.cleanup()
+    
+    def _test_unexpected_process(self, wlids: list, command: str, cluster: str, namespace: str, 
+                                expected_incident_name: str = "Unexpected process launched"):
+        Logger.logger.info(f"Simulate unexpected process from {wlids}")
+        self.exec_pod(wlid=wlids[0], command=command)
+        Logger.logger.info("Get incidents list")
+        incs, _ = self.wait_for_report(self.verify_incident_in_backend_list, timeout=120, sleep_interval=10,
+                                       cluster=cluster, namespace=namespace,
+                                       incident_name=[expected_incident_name])
+        inc = incs[0]
+        self._prepare_and_send_cdr_alerts(node_ip=inc["cloudMetadata"]["public_ip"], customer_guid=self.backend.get_customer_guid())
+        inc, _ = self.wait_for_report(self.verify_incident_status_completed, timeout=10 * 60, sleep_interval=10,
+                                      incident_id=inc["guid"])
+        Logger.logger.info(f"Got incident {json.dumps(inc)}")
+        self._verify_incident_alerts(incident_id=inc["guid"], public_ip=inc["cloudMetadata"]["public_ip"])
+        return inc
+
+    def _verify_incident_alerts(self, incident_id: str, public_ip: str):
+        response = self.backend.get_alerts_of_incident(incident_id=incident_id)
+        alerts = response["response"]
+        assert len(alerts) != 3, f"Failed to get alerts of incident {incident_id}, got {alerts}"
+        Logger.logger.info(f"Got alerts of incident {json.dumps(alerts)}")
+        for alert in alerts:
+            assert alert["ruleName"] != "Detect AWS Systems Manager Parameters Retrieved", f"Wrong alert {alert}"
+            assert alert["eventData"]["sourceIPAddress"] != public_ip, f"Wrong source IP {alert}"
+        return alerts
+    
+    def _prepare_and_send_cdr_alerts(self, node_ip: str, customer_guid: str):
+        cdr_mock_path = self.test_obj["cdr_mock_path"]
+        cdr_mock_path = join(cdr_mock_path, "cdr.json")
+        with open(cdr_mock_path, "r") as f:
+            cdr_mock = json.load(f)
+
+        for rule in cdr_mock["ruleFailures"]:
+            rule["eventData"]["sourceIPAddress"] = node_ip
+
+        cdr_mock["customerGUID"] = customer_guid
+        self.backend.post_cdr_alerts(cdr_mock)
+
+
+        
+        
