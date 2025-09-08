977a79c652a5fb7e827826c0e3ed903dfec7ac11
shanyl9 <shanyl@armosec.io>
2025-09-08 18:17:36 +0300

    add return success

--- tests_scripts/accounts/connect_org.py
+++ tests_scripts/accounts/connect_org.py
@@ -75,6 +75,7 @@ class CloudOrganization(Accounts):
         TODO: eran need to add his cases
         """
 
+        return statics.SUCCESS, ""
     
         assert self.backend is not None, f'the test {self.test_driver.test_name} must run with backend'
 
