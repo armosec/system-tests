845b97b1c48ef5e144d88869f50d8677f38f0658
kooomix <eranm@armosec.io>
2025-09-15 11:06:39 +0300

    Increase timeout duration in CloudVulnScan class from 840 to 960 seconds for improved vulnerability scan completion.

--- tests_scripts/accounts/vulnscan.py
+++ tests_scripts/accounts/vulnscan.py
@@ -91,7 +91,7 @@ class CloudVulnScan(Accounts):
         Logger.logger.info('Stage 4: Wait for cspm vulnscan scan to complete successfully')
         result, _ = self.wait_for_report(
             self.validate_hosts_list,
-            timeout=840,
+            timeout=960,
             sleep_interval=20,
             cloud_account_guid=cloud_account_guid,
             expected_instances_ids=expected_instances_ids
