0596a7b38d2a52229546b370ade59fa253853b80
kooomix <emadar@gmail.com>
2025-10-09 09:18:18 +0300

    Revert "ka"

--- tests_scripts/accounts/vulnscan.py
+++ tests_scripts/accounts/vulnscan.py
@@ -52,7 +52,7 @@ class CloudVulnScan(Accounts):
        
         """
 
-        return statics.SUCCESS, ""
+        #return statics.SUCCESS, ""
 
         assert self.backend is not None, f'the test {self.test_driver.test_name} must run with backend'
 
