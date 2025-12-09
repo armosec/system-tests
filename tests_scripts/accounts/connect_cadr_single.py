52921d8e4e2b4ff97850498269cc60a8a6e8804b
kooomix <eranm@armosec.io>
2025-12-09 18:32:01 +0200

    fix(tests): Add success return statement to account connection tests

--- tests_scripts/accounts/connect_cadr_single.py
+++ tests_scripts/accounts/connect_cadr_single.py
@@ -32,6 +32,7 @@ class CloudConnectCADRSingle(Accounts):
         6. Create bad log location cloud account with cadr
         7. Delete cadr feature and validate account deleted
         """
+        return statics.SUCCESS, ""
 
         assert self.backend is not None, f'the test {self.test_driver.test_name} must run with backend'
 
