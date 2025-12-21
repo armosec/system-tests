b65dfdbdae4c749dbcb892e88d7f89ae761472b9
idoh <idoh@armosec.io>
2025-12-21 16:34:33 +0200

    add cloud prefic

--- configurations/system/tests_cases/accounts_tests.py
+++ configurations/system/tests_cases/accounts_tests.py
@@ -15,7 +15,7 @@ class AccountsTests(object):
 
     @staticmethod
     def cloud_connect_azure_cspm_single():
-        from tests_scripts.accounts.connect_cspm_single_azure import CloudConnectCSPMSingleAzure
+        from tests_scripts.accounts.cloud_connect_cspm_single_azure import CloudConnectCSPMSingleAzure
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudConnectCSPMSingleAzure,
@@ -23,7 +23,7 @@ class AccountsTests(object):
         )
     @staticmethod
     def cloud_connect_gcp_cspm_single():
-        from tests_scripts.accounts.connect_cspm_single_gcp import CloudConnectCSPMSingleGCP
+        from tests_scripts.accounts.cloud_connect_cspm_single_gcp import CloudConnectCSPMSingleGCP
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudConnectCSPMSingleGCP,
@@ -32,7 +32,7 @@ class AccountsTests(object):
 
     @staticmethod
     def cloud_connect_aws_cspm_single():
-        from tests_scripts.accounts.connect_cspm_single_aws import CloudConnectCSPMSingleAWS
+        from tests_scripts.accounts.cloud_connect_cspm_single_aws import CloudConnectCSPMSingleAWS
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudConnectCSPMSingleAWS,
@@ -41,7 +41,7 @@ class AccountsTests(object):
 
     @staticmethod
     def cloud_connect_aws_cadr_single():
-        from tests_scripts.accounts.connect_cadr_single import CloudConnectCADRSingle
+        from tests_scripts.accounts.cloud_connect_cadr_single_aws import CloudConnectCADRSingle
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudConnectCADRSingle,
@@ -50,7 +50,7 @@ class AccountsTests(object):
 
     @staticmethod
     def cloud_organization_aws_cspm():
-        from tests_scripts.accounts.connect_cspm_org import CloudOrganizationCSPM
+        from tests_scripts.accounts.cloud_connect_cspm_org_aws import CloudOrganizationCSPM
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudOrganizationCSPM,
@@ -59,7 +59,7 @@ class AccountsTests(object):
     
     @staticmethod
     def cloud_organization_aws_cadr():
-        from tests_scripts.accounts.connect_cadr_org import CloudOrganizationCADR
+        from tests_scripts.accounts.cloud_connect_cadr_org_aws import CloudOrganizationCADR
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudOrganizationCADR,
@@ -68,7 +68,7 @@ class AccountsTests(object):
     
     @staticmethod
     def cloud_vulnscan_aws():
-        from tests_scripts.accounts.vulnscan import CloudVulnScan
+        from tests_scripts.accounts.cloud_vulnscan_aws import CloudVulnScan
         return KubescapeConfiguration(
             name=inspect.currentframe().f_code.co_name,
             test_obj=CloudVulnScan,
